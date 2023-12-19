1f87aac0c4231cc75a3edd0d2dec0629903e23f5
João Vitor Pereira <joao.joaovitorp481@gmail.com>
2023-12-19 17:40:06 -0300

    Adicionando função de adicionar produto ao carrinho e formatação do código

--- Magazine Hashtag/main.js
+++ Magazine Hashtag/main.js
@@ -1,56 +1,5 @@
-import { inicializarCarrinho } from "./src/menuCarrinho"
+import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarCarrinho } from "./src/menuCarrinho";
 
-const catalogo =[{
-    id: 1,
-    nome: 'Moletom Enrugado',
-    marca: 'Zara',
-    preco: 70,
-    nomeArquivoImagem: 'moletom_enrugado.jpg'
-    },
-    {
-    id: 2,
-    nome: 'Moletom Branco',
-    marca: 'Zara',
-    preco: 75,
-    nomeArquivoImagem: 'moletom_branco.jpg'
-    },
-    {
-    id: 3,
-    nome: 'Moletom com Pespontos',
-    marca: 'Zara',
-    preco: 70,
-    nomeArquivoImagem: 'moletom_pespontos.jpg'
-    },
-    {
-    id: 4,
-    nome: 'Moletom Verde',
-    marca: 'Zara',
-    preco: 80,
-    nomeArquivoImagem: 'moletom_verde.jpg'
-    },
-    {
-    id: 5,
-    nome: 'Moletom Azul',
-    marca: 'Zara',
-    preco: 65,
-    nomeArquivoImagem: 'moletom_azul.jpg'
-    }
-]
-
-for(const produtoCatalogo of catalogo){
-    const cartaoProduto= `<div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
-    <img 
-        src="assets/img/${produtoCatalogo.nomeArquivoImagem}"
-        alt="Produto 1 do Magazine Hashtag."
-        style="height: 300px"
-    />
-    <p class='marca'>${produtoCatalogo.marca}</p>
-    <p>${produtoCatalogo.nome}</p>
-    <p>$${produtoCatalogo.preco}</p>
-    <button>Adicionar</button>
-    </div>`
-    
-    document.getElementById('container-produto').innerHTML += cartaoProduto
-}
-
+renderizarCatalogo();
 inicializarCarrinho();
---
+++ Magazine Hashtag/src/utilidades.js
@@ -0,0 +1,41 @@
+export const catalogo =[{
+    id: 1,
+    nome: 'Moletom Enrugado',
+    tamanho: 'M',
+    marca: 'Zara',
+    preco: 70,
+    nomeArquivoImagem: 'moletom_enrugado.jpg'
+    },
+    {
+    id: 2,
+    nome: 'Moletom Branco',
+    tamanho: 'G',
+    marca: 'Zara',
+    preco: 75,
+    nomeArquivoImagem: 'moletom_branco.jpg'
+    },
+    {
+    id: 3,
+    nome: 'Moletom com Pespontos',
+    tamanho: 'P',
+    marca: 'Zara',
+    preco: 70,
+    nomeArquivoImagem: 'moletom_pespontos.jpg'
+    },
+    {
+    id: 4,
+    nome: 'Moletom Verde',
+    tamanho: 'PP',
+    marca: 'Zara',
+    preco: 80,
+    nomeArquivoImagem: 'moletom_verde.jpg'
+    },
+    {
+    id: 5,
+    nome: 'Moletom Azul',
+    tamanho: 'GG',
+    marca: 'Zara',
+    preco: 65,
+    nomeArquivoImagem: 'moletom_azul.jpg'
+    }
+];
