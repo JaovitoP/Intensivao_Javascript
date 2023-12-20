f0050f02efdbbbfca5857f8df9db22424358bc9f
João Vitor Pereira <joao.joaovitorp481@gmail.com>
2023-12-20 18:05:43 -0300

    Adicionando mais produtos à loja

--- Magazine Hashtag/src/utilidades.js
+++ Magazine Hashtag/src/utilidades.js
@@ -1,18 +1,18 @@
 export const catalogo =[{
     id: '1',
-    nome: 'Moletom Enrugado',
-    tamanho: 'M',
+    nome: 'Moletom Branco',
+    tamanho: 'P',
     marca: 'Zara',
     preco: 70,
-    nomeArquivoImagem: 'moletom_enrugado.jpg'
+    nomeArquivoImagem: 'moletom_branco.jpg'
     },
     {
     id: '2',
-    nome: 'Moletom Branco',
+    nome: 'Moletom Verde',
     tamanho: 'G',
     marca: 'Zara',
     preco: 75,
-    nomeArquivoImagem: 'moletom_branco.jpg'
+    nomeArquivoImagem: 'moletom_verde.jpg'
     },
     {
     id: '3',
@@ -24,18 +24,91 @@ export const catalogo =[{
     },
     {
     id: '4',
-    nome: 'Moletom Verde',
+    nome: 'Macacão Denim',
     tamanho: 'PP',
     marca: 'Zara',
     preco: 80,
-    nomeArquivoImagem: 'moletom_verde.jpg'
+    nomeArquivoImagem: 'macacao_denim.jpg'
     },
     {
     id: '5',
-    nome: 'Moletom Azul',
+    nome: 'Macacão jeans',
     tamanho: 'GG',
     marca: 'Zara',
     preco: 65,
+    nomeArquivoImagem: 'macacao_jeans.jpg'
+    },
+    {
+    id: '6',
+    nome: 'Moletom Enrugado',
+    tamanho: 'M',
+    marca: 'Zara',
+    preco: 70,
+    nomeArquivoImagem: 'moletom_enrugado.jpg'
+    },
+    {
+    id: '7',
+    nome: 'Moletom Azul',
+    tamanho: 'G',
+    marca: 'Zara',
+    preco: 75,
     nomeArquivoImagem: 'moletom_azul.jpg'
+    },
+    {
+    id: '8',
+    nome: 'Casaco com Alamares',
+    tamanho: 'G',
+    marca: 'Zara',
+    preco: 80,
+    nomeArquivoImagem: 'casaco_alamares.jpg'
+    },
+    {
+    id: '9',
+    nome: 'Casaco Cruzado',
+    tamanho: 'G',
+    marca: 'Zara',
+    preco: 75,
+    nomeArquivoImagem: 'casaco_cruzado.jpg'
+    },
+    {
+    id: '10',
+    nome: 'Vestido de Algodão',
+    tamanho: 'G',
+    marca: 'Zara',
+    preco: 90,
+    nomeArquivoImagem: 'vestido_algodao.jpg'
+    },
+    {
+    id: '11',
+    nome: 'Trench',
+    tamanho: 'GG',
+    marca: 'Zara',
+    preco: 150,
+    nomeArquivoImagem: 'trench.jpg'
+    },
+    {
+    id: '12',
+    nome: 'Brinco de Argola',
+    tamanho: 'GG',
+    marca: 'Zara',
+    preco: 65,
+    nomeArquivoImagem: 'brinco_argola.jpg'
+    },
+    {
+    id: '13',
+    nome: 'Boné de Sarja',
+    tamanho: 'P',
+    marca: 'Zara',
+    preco: 30,
+    nomeArquivoImagem: 'bone_sarja.jpg'
+    },
+    {
+    id: '14',
+    nome: 'Corrente com bolinhas',
+    tamanho: 'PP',
+    marca: 'Zara',
+    preco: 20,
+    nomeArquivoImagem: 'corrente_bolinhas.jpg'
     }
+    
 ];
